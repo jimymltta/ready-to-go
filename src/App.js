aa6ae78268cb7fab541e68f6e40427fe3ee46ee9
jimymltta <marlettajimy@gmail.com>
2021-07-21 22:13:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
+import { span } from "prelude-ls";
 import "./App.css";
+// Import de useState
+import { useState } from "react";
 
 function App() {
+  const [switch1, setSwitch1] = useState(false);
+  const [switch2, setSwitch2] = useState(false);
+  const [switch3, setSwitch3] = useState(false);
+  const [switchOff, setSwitchOff] = useState(true);
+
+  const changeState1 = () => {
+    const newState = true;
+    setSwitch1(newState);
+  };
+
+  const changeState2 = () => {
+    const newState = true;
+    setSwitch2(newState);
+  };
+
+  const changeState3 = () => {
+    const newState = true;
+    setSwitch3(newState);
+  };
+
+  const changeSwitchOff = () => {
+    const newState = false;
+    setSwitchOff(newState);
+  };
+
   return (
     <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+      <div className="switch1">
+        <button onClick={changeState1}>ON</button>
+        <button onClick={changeSwitchOff}>OFF</button>
+      </div>
+
+      <div className="switch2">
+        <button onClick={changeState2}>ON</button>
+        <button onClick={changeSwitchOff}>OFF</button>
+      </div>
+
+      <div className="switch3">
+        <button onClick={changeState3}>ON</button>
+        <button onClick={changeSwitchOff}>OFF</button>
+      </div>
+
+      <div className="result">
+        {switch1 && switch2 && switch3 ? (
+          <span>Go !</span>
+        ) : (
+          <span>No way !</span>
+        )}
+      </div>
     </div>
   );
 }
